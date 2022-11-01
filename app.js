3a9115e4f475b41db6dd37747c86d6f08c7c0b14
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-01 14:43:59 +0000

    saved deleting

--- app.js
+++ app.js
@@ -1,30 +1,38 @@
 
 class typeEffect{
-    constructor(words, displayArea){
+    constructor(displayArea, words, ){
+        this.displayArea = displayArea
         this.words = words;
         this.text ='';
         this.index = 0;
         this.type();
         this.isDeleting = false;
-        this.displayArea = displayArea
     }
     type(){
         const current = this.index % this.words.length;
+        console.log(this.words);
         let eachText = this.words[current];
+        console.log(eachText);
         if (this.isDeleting) {
             this.text = eachText.substring(0, this.text.length -1)
 
         }else{
             this.text = eachText.substring(0, this.text.length + 1)
+            console.log(this.text);
         }
         this.displayArea.innerHTML = `<span class="txt">${this.text}</span>`;
-        this.type()
+        if (this.isDeleting  && this.text === eachText) {
+            this.isDeleting = true
+            
+        }
+      
     }
 }
 function intialize() {
     const data = ["Software Developer", "Frontend Developer", "Blockchain Developer"];
     const displayArea = document.querySelector('.text-container');
 
-    new typeEffect(displayArea, data)
+    const kk = new typeEffect(displayArea, data)
+    kk.type()
 }
 document.addEventListener("DOMContentLoaded", intialize)
