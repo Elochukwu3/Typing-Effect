693503aa977f1e10c2635a8cf40072ed25456c7e
Elochukwu3 <oguajuromauld5@gmail.com>
2022-11-01 14:00:36 +0000

    saved files

---
+++ app.js
@@ -0,0 +1,13 @@
+
+class typeEffect{
+    constructor(words){
+        this.words = words;
+        this.text ='';
+        this.index = 0;
+        this.type();
+        this.isDeleting = false;
+    }
+    type(){
+        
+    }
+}
